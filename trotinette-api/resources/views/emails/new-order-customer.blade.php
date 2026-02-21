896a9a8824531ffd0dfe4faef7f14a9ad120888d
Nabil Dorhmi <nabil.doghmi@reecho.eu>
2026-02-21 20:14:14 +0000

    feat(quick-8): create queued mailable classes for order notifications
    - NewOrderCustomer for customer order creation notification (pending status)
    - NewOrderAdmin for admin new order notification
    - OrderConfirmed for order confirmation notification
    - OrderCancelled for order cancellation notification (with optional reason)
    - All implement ShouldQueue and use onQueue('emails')
    - French markdown email views with order details (number, items, total, status)

---
+++ trotinette-api/resources/views/emails/new-order-customer.blade.php
@@ -0,0 +1,34 @@
+<x-mail::message>
+# Confirmation de commande
+
+Bonjour {{ $order->user->name }},
+
+Nous avons bien reçu votre commande **{{ $order->order_number }}**. Votre commande est actuellement en attente de confirmation.
+
+## Détails de la commande
+
+**Statut:** {{ $order->status->label('fr') }}
+**Téléphone:** {{ $order->phone }}
+**Ville:** {{ $order->city }}
+
+<x-mail::table>
+| Produit | Quantité | Prix unitaire | Sous-total |
+|:--------|:--------:|:-------------:|-----------:|
+@foreach($order->items as $item)
+| {{ $item->product->name ?? $item->product_sku }} | {{ $item->quantity }} | {{ number_format($item->unit_price / 100, 2) }} MAD | {{ number_format($item->subtotal / 100, 2) }} MAD |
+@endforeach
+</x-mail::table>
+
+**Sous-total:** {{ number_format($order->subtotal / 100, 2) }} MAD
+**Frais de livraison:** {{ number_format($order->delivery_fee / 100, 2) }} MAD
+**Total:** {{ number_format($order->total / 100, 2) }} MAD
+
+@if($order->note)
+**Note:** {{ $order->note }}
+@endif
+
+Nous vous contacterons bientôt pour confirmer votre commande.
+
+Merci pour votre confiance,<br>
+{{ config('app.name') }}
+</x-mail::message>
